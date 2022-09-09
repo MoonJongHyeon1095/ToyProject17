5d60c1fa60ae770cd3c38007d11c53fdd2a89bf7
Minhyeong-S <alsuddl25@icloud.com>
2022-09-09 12:06:41 +0900

    app.py 수정한 것 이전 커밋에서 빠져서 다시 커밋( 이전 커밋 내용 참고)

--- toyproject/app.py
+++ toyproject/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify, redirect
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+import certifi
+ca = certifi.where()
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.j6ve73r.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -10,16 +12,62 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(
+        'https://search.shopping.naver.com/book/search?bookTabType=BEST_SELLER&catId=50005542&pageIndex=1&pageSize=40&query=%EB%84%A4%EC%9D%B4%EB%B2%84%EB%8F%84%EC%84%9C&sort=REL',
+        headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    bookranks = soup.select('#book_list > ul > li')
+
+    all_Brank = list(db.bookrank.find({}, {'_id': False}))
+    for book in all_Brank:
+        db.bookrank.delete_one({'title': book['title']})
+
+
+    count = 0
+    for i in bookranks:
+        count += 1
+        if count > 3:
+            break
+
+        a = i.select_one(
+            'div > a.bookListItem_info_top__VgpiO.linkAnchor > div.bookListItem_text_area__hF892 > div.bookListItem_title__X7f9_ > span')
+        title = a.text.split('위')[1]
+        rank = a.text.split('위')[0]
+        writer = i.select_one(
+            'div > a.bookListItem_info_top__VgpiO.linkAnchor > div.bookListItem_text_area__hF892 > div.bookListItem_detail__RBQ6x > span.bookListItem_author__fdjcn').text[
+                 2:]
+        publisher = i.select_one(
+            'div > a.bookListItem_info_top__VgpiO.linkAnchor > div.bookListItem_text_area__hF892 > div.bookListItem_detail__RBQ6x > span.bookListItem_detail_publish__FgPYQ > span:nth-child(1)').text[
+                    2:]
+
+        doc = {
+            'rank': rank,
+            'title': title,
+            'writer': writer,
+            'publisher': publisher
+
+        }
+        db.bookrank.insert_one(doc)
+
+
+    last_rank = list(db.bookrank.find({}, {'_id': False}))
     books = list(db.books.find({}, {'_id': False}))
 
     print(books)
     # return jsonify({'books':books})
-    return render_template('index.html', books=books)
+    return render_template('test1.html', books=books, ranks=last_rank)
 
 
+
+
+# 메인에서 '마음공유' 버튼 클릭 후 url 입력했을 때!
+# 유얄엘 요청은 /review (method는 get) 로 받지만 뿌리는 html은 detail임.
 @app.route("/review", methods=["GET"])
 def book_get():
-    url = request.args.get('bUrl')
+    url = request.args.get('bUrl')  # get 요청 받아오는 방법 : request.arg.get('form의 name')
     # https%3A%2F%2Fsearch.shopping.naver.com%2Fbook%2Fcatalog%2F32467089683%3FNaPm%3Dct%253Dl7mysnhs%257Cci%253Dd227deb74b6ba5a8436201854d336741d7ccb875%257Ctr%253Dboksl%257Csn%253D95694%257Chk%253D71a0581f878b44a2e5c214424d595ac1724f4337
     # print(url)
 
@@ -42,9 +90,12 @@ def book_get():
     # return jsonify({'url':url, 'title':title, 'image':image})
     return render_template('detail.html', title=title, image=image)
 
+
+
+# 마음공유 버튼 통해서 유알엘 입력 > 별점선택/리뷰작성 후 '등록하기'버튼 눌렀을 때!
 @app.route("/review", methods=["POST"])
 def book_post():
-    image = request.form['image']
+    image = request.form['image']       # post 요청 받아오는 방법 : request.form['form의 name']
     title = request.form['title']
     star = request.form['star']
     content = request.form['content']
@@ -62,9 +113,9 @@ def book_post():
         'content': content
     }
 
-    db.books.insert_one(doc);
+    db.books.insert_one(doc)
 
-    return redirect('/');
+    return redirect('/')
     # return render_template('index.html')
 
 # localhost:5000/1
